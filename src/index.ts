83f0a5b57ecf5499d85608e97ea3d92da4a20558
liusize <liusize@gdrlkjyxgs9.wecom.work>
2023-11-28 20:10:07 +0800

    :bookmark: 0.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
+export { default as Delete } from './Delete';
 export { default as Drawer } from './Drawer';
 export { default as Modal } from './Modal';
 export { default as TreeSelect } from './TreeSelect';
