e2a286153b772f814095f304ada6f3a7f293fd0a
liusize <liusize@gdrlkjyxgs9.wecom.work>
2023-12-15 10:27:34 +0800

    :art: 优化删除按钮逻辑

--- src/Delete/Button.tsx
+++ src/Delete/Button.tsx
@@ -1,25 +1,55 @@
 import { DeleteOutlined } from '@ant-design/icons';
+import { isPromise } from '@hudiemon/utils';
 import {
   Button as AntButton,
   ButtonProps as AntButtonProps,
+  message,
   ModalFuncProps,
 } from 'antd';
 import confirm from './confirm';
 
 type ButtonProps = AntButtonProps & {
   confirmProps?: ModalFuncProps;
+  request?: () => Promise<void>;
 };
 const Button = (props: ButtonProps) => {
-  const { confirmProps = {}, ...buttonProps } = props;
+  const { confirmProps, request, ...buttonProps } = props;
   return (
-    <AntButton
-      danger
-      icon={<DeleteOutlined />}
-      {...buttonProps}
-      onClick={() => confirm(confirmProps)}
-    >
-      删除
-    </AntButton>
+      <AntButton
+          danger
+          icon={<DeleteOutlined />}
+          {...buttonProps}
+          onClick={() =>
+              confirm({
+                ...confirmProps,
+                onOk: async () => {
+                  if (confirmProps?.onOk) {
+                    const promise = confirmProps?.onOk();
+                    if (isPromise(promise)) {
+                      try {
+                        await promise;
+                      } catch (e) {
+                        throw e;
+                      }
+                    }
+                  }
+                  if (request) {
+                    const promise = request();
+                    if (isPromise(promise)) {
+                      try {
+                        await promise;
+                        message.success('删除成功');
+                      } catch (e) {
+                        throw e;
+                      }
+                    }
+                  }
+                },
+              })
+          }
+      >
+        删除
+      </AntButton>
   );
 };
 
